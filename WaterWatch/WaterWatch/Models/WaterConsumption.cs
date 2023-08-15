d6e21059307edb5bd2b657e3e1668ec5ec83d964
Joana Carvalho <36134456+Joanarfc@users.noreply.github.com>
2023-08-15 09:40:59 -0300

    Added WaterConsumption model

---
+++ WaterWatch/WaterWatch/Models/WaterConsumption.cs
@@ -0,0 +1,12 @@
+
+namespace WaterWatch.Models
+{
+    public class WaterConsumption
+    {
+        public int id { get; set;}
+        public string neighborhood { get; set;}
+        public string suburb_group { get; set;}
+        public int averageMonthlyKL { get; set;}
+        public string coordinates { get; set;}
+    }
+}
